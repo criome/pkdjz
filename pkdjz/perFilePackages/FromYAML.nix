dcf187c4bf29622cbb1bd459e7d586a400d7eb59
li <li@maisiliym.criome.me>
2025-02-14 21:18:08 +0100

    modified(fromYAML use remarshal)

--- pkdjz/perFilePackages/FromYAML.nix
+++ pkdjz/perFilePackages/FromYAML.nix
@@ -1,13 +1,12 @@
-{ yj, runCommandLocal }:
+{ remarshal, runCommandLocal }:
 
 YAMLString:
 let
-  formatFlag = "-yj";
-  prettyFlag = "-i";
-  convertCmd = "${yj}/bin/yj ${formatFlag} ${prettyFlag}";
+  formatFlags = "--from yaml --to json";
+  convertCmd = "${remarshal}/bin/remarshal ${formatFlags} --input -";
 
   jsonFile = runCommandLocal "FromYamlArtefact" { inherit YAMLString; } ''
-    printf '%s' """$YAMLString""" | ${convertCmd} > $out
+    printf '%s' """$YAMLString""" | ${convertCmd} --output $out
   '';
 
   jsonString = std.readFile jsonFile;
